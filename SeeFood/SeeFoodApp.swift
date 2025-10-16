cdff72f8e25cd1b247092b1014a940e7ffdf78ea
faraz2079 <farazchahi2079@gmail.com>
2025-10-16 20:30:06 +0200

    Initial Commit

---
+++ SeeFood/SeeFoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeeFoodApp.swift
+//  SeeFood
+//
+//  Created by Faraz on 10/16/25.
+//
+
+import SwiftUI
+
+@main
+struct SeeFoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
