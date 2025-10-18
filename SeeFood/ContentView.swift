f159f5575bed772f9dc2b4fee2430093bfbcb40f
faraz2079 <farazchahi2079@gmail.com>
2025-10-18 11:38:16 +0200

    Navbar added

--- SeeFood/ContentView.swift
+++ SeeFood/ContentView.swift
@@ -1,13 +1,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showCamera = false
+    @State private var image: UIImage?
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        ZStack {
+            if let image {
+                Image(uiImage: image)
+                    .resizable()
+                    .ignoresSafeArea()
+                    .scaledToFill()
+            } else {
+                Text("Tap the camera button to capture an image")
+            }
+        }.navigationTitle(showCamera ? "Processing..." : "Camera")
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        showCamera = true
+                    } label: {
+                        Image(systemName: "camera.fill")
+                    }
+                    .accessibilityLabel("Open Camera")
+                }
+            }
+            .sheet(isPresented: $showCamera) {
+                ContentView() // change to CameraView
+            }
     }
+}
+
+#Preview {
+    ContentView()
 }
